8da2d8eb00d6766a3c6960cae5dfbffbca92a4bb
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2022-06-01 15:24:23 +0100

    make solar system class base

---
+++ Graphics Programming/Graded/solarSystem/CelestialBody.js
@@ -0,0 +1,60 @@
+class CelestialBody
+{
+	constructor(sprite, color, bodyRadius, name,
+		rotationSpeed, orbitRadius, orbitSpeed)
+	{
+		this.Sprite = sprite;
+		this.Color = color;
+		this.BodyRadius = bodyRadius;
+		this.Name = name;
+
+		this.RotationSpeed = rotationSpeed;
+		this.OrbitRadius = orbitRadius;
+		this.OrbitSpeed = orbitSpeed;
+
+		this.Satellites = [];
+	}
+
+	AddSatellite(satellite)
+	{
+		this.Satellites.push(satellite);
+	}
+
+	DrawBodyAndSatellites()
+	{
+		push();
+
+		rotate(radians(Time * this.RotationSpeed));
+		translate(this.OrbitRadius, 0);
+		rotate(radians(Time * this.OrbitSpeed));
+
+		this.DrawBody();
+
+
+		for (let i = 0; i < this.Satellites.length; i++)
+		{
+			this.Satellites[i].DrawBodyAndSatellites();
+		}
+
+		pop();
+	}
+
+	DrawBody()
+	{
+		strokeWeight(5);
+		fill(this.Color);
+		stroke(0);
+
+		ellipse(0, 0, this.BodyRadius, this.BodyRadius);
+
+		this.Sprite.Draw(0, 0, this.BodyRadius, this.BodyRadius, true)
+		line(0, 0, this.BodyRadius/2, 0);
+
+		push();
+		fill(color(255,255,255));
+		noStroke(51);
+		text(this.Name, this.BodyRadius/2, 0);
+		pop();
+	}
+
+}
--- Graphics Programming/Graded/solarSystem/sketch.js
+++ Graphics Programming/Graded/solarSystem/sketch.js
@@ -1,11 +1,7 @@
-var Speed;
+var Time;
 
 var Background;
-var Sun;
-var Mercury;
-var Earth;
-var Moon1;
-var Moon2;
+var SystemCenterBody;
 
 function preload()
 {
@@ -16,11 +12,33 @@ function preload()
 	let xSpriteCount = 10;
 	let ySpriteCount = 20;
 	let timePerFrame = 1 / 30;
-	Sun = new AnimatedSprite("Assets/Sun.png", xSpriteCount, ySpriteCount, timePerFrame, 2);
-	Mercury = new AnimatedSprite("Assets/Mercury.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
-	Earth = new AnimatedSprite("Assets/Earth.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
-	Moon1 = new AnimatedSprite("Assets/Moon1.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
-	Moon2 = new AnimatedSprite("Assets/Moon2.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
+
+	// load CelestialBody assets
+	let sunSprite = new AnimatedSprite("Assets/Sun.png", xSpriteCount, ySpriteCount, timePerFrame, 2);
+	let mercurySprite = new AnimatedSprite("Assets/Mercury.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
+	let earthSprite = new AnimatedSprite("Assets/Earth.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
+	let moon1Sprite = new AnimatedSprite("Assets/Moon1.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
+	let moon2Sprite = new AnimatedSprite("Assets/Moon2.png", xSpriteCount, ySpriteCount, timePerFrame, 1);
+
+	// setup each Celestial Body in the system
+	SystemCenterBody = new CelestialBody(sunSprite, color(255,150,0), 200, "Sun",
+		1/3, 0, 0);
+
+	let mercury = new CelestialBody(mercurySprite, color(255,0,0), 60, "Mercury",
+		1.25, 200, 1);
+	SystemCenterBody.AddSatellite(mercury);
+
+	let earth = new CelestialBody(earthSprite, color(0,0,255), 80, "Earth",
+		1, 300, 1);
+	SystemCenterBody.AddSatellite(earth);
+
+	let moon1 = new CelestialBody(moon1Sprite, color(255,255,255), 30, "Moon",
+		-2, 100, 0);
+	earth.AddSatellite(moon1);
+	let moon2 = new CelestialBody(moon2Sprite, color(200,200,200), 20, "Moon 2",
+		-2.5, 70, 0);
+	earth.AddSatellite(moon2);
+
 }
 
 function setup()
@@ -33,52 +51,10 @@ function draw()
 {
 	background(0);
 	image(Background, 0, 0)
-	Speed = frameCount;
+	Time = frameCount;
 
-	push();
+	// center system in middle of the canvas
 	translate(width/2, height/2);
-	rotate(radians(Speed/3));
-	celestialObj(color(255,150,0), 200, Sun, "Sun"); // Sun
 
-	push();
-	rotate(radians(Speed));
-	translate(300, 0);
-	rotate(radians(Speed));
-	celestialObj(color(0,0,255), 80, Earth, "Earth"); // Earth
-
-	push();
-	rotate(radians(-Speed*2));
-	translate(100, 0);
-	celestialObj(color(255,255,255), 30, Moon1, "Moon"); // Moon
-
-	pop();
-	rotate(radians(-Speed*2.5));
-	translate(70, 0);
-	celestialObj(color(255,255,255), 20, Moon2, "Moon 2"); // Moon 2
-
-	pop();
-
-	push();
-	rotate(radians(Speed*1.25+180))
-	translate(200, 0);
-	rotate(radians(Speed));
-	celestialObj(color(255,0,0), 60, Mercury, "Mercury"); // Mercury
-}
-
-function celestialObj(c, size, sprite, name)
-{
-	strokeWeight(5);
-	fill(c);
-	stroke(0);
-
-	ellipse(0, 0, size, size);
-
-	sprite.Draw(0, 0, size, size, true)
-	line(0, 0, size/2, 0);
-
-	push();
-	fill(color(255,255,255));
-	noStroke(51);
-	text(name, size/2, 0);
-	pop();
+	SystemCenterBody.DrawBodyAndSatellites();
 }
